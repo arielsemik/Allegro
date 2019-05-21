9606241424149207ade9ebffa0be6b1007d06df2
Ariel Semik <semikariel@gmail.com>
2019-05-21 23:31:23 +0200

    Initial commit

---
+++ products/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(ProductImages)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['cat_name_father', 'cat_name']
+
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin ):
+    list_display = ['product_category','name','stock', 'price']
+admin.site.register(Product, ProductAdmin)
---
+++ sprzedawca/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SprzedawcaConfig(AppConfig):
+    name = 'sprzedawca'

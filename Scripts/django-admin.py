bd70f381e77db4f4092f60d160957164071cda17
Ariel Semik <semikariel@gmail.com>
2019-05-23 00:51:54 +0200

    PY-2019.1.2 <semik@AS-Komp Overwrite remote https://github.com/arielsemik/Allegro.git to local

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\semik\Dysk Google\Programowanie\Kurs Python\Allegro\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
